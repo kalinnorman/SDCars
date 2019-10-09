00b72302b05bc10ee5cec8d176d2e5d029996343
redd <bryan.d.redd@gmail.com>
2019-10-09 17:14:25 -0600

    Wasn't importing entire file, so serial communications wasn't established.

--- class_code/demo_steering.py
+++ class_code/demo_steering.py
@@ -7,7 +7,7 @@ This is a demo function for controlling the car.
 If this script this run as the main, this will initialize the car and tell it to turn the wheels and drive.
 """
 
-from car_control import steer, drive, initialize_car
+import car_control as cc
 import time
 
 
@@ -17,30 +17,30 @@ def steering_commands():
     :return:
     """
     print("right")
-    steer(15.0)
+    cc.steer(15.0)
     time.sleep(2)
     print("left")
-    steer(-15.0)
+    cc.steer(-15.0)
     time.sleep(2)
     print("go")
-    drive(0.5)
+    cc.drive(0.5)
     time.sleep(2)
     print("stop")
-    drive(0.0)
+    cc.drive(0.0)
     time.sleep(2)
 
 
 if __name__ == '__main__':
 
     # initialize communication with the Arduino
-    initialize_car(pid=False)
+    cc.initialize_car(pid=False)
 
     # run the loop, waiting for a keyboard interrupt
     try:
+        print("Beginning loop")
         while True:
-            print("Loop")
             steering_commands()  # run the sequence of steering commands
     except KeyboardInterrupt:
-        drive(0.0)  # stop the car
-        steer(0.0)  # return wheels to normal
+        cc.drive(0.0)  # stop the car
+        cc.steer(0.0)  # return wheels to normal
         print("User stopped the script. (KeyboardInterrupt)")
