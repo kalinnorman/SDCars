0ac7e4cbd17d8a13a45349c75a938e00f36ea933
redd <bryan.d.redd@gmail.com>
2019-10-09 18:36:12 -0600

    asfd

--- class_code/driveTesting2.py
+++ class_code/driveTesting2.py
@@ -2,16 +2,19 @@ import serial
 import time
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
+time.sleep(3)
 ser.flushInput()
-
+time.sleep(3)
 ser.write("!start1600\n".encode())
-time.sleep(1)
+time.sleep(3)
 
 try:
     print("Starting Loop")
     while True:
+        print("go")
         ser.write("!speed0\n".encode())
         time.sleep(1)
+        print("stop")
         ser.write("!speed1\n".encode())
         time.sleep(1)
 except KeyboardInterrupt:
