3493e12f788259f3f4a45d523c5838dd46a2053c
redd <bryan.d.redd@gmail.com>
2019-10-10 17:37:28 -0600

    misc

--- class_code/demo_steering.py
+++ class_code/demo_steering.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
             cc.update_sensors()
             t, rgb = cc.get_rgb_data()  # get color image
             t, depth = cc.get_depth_data()  # get depth data
-            depth_scaled = ((depth / np.amax(depth)) * 255).astype(dtype='uint8')
+            depth_scaled = ((depth / np.median(depth)) * 128).astype(dtype='uint8')
             depth_scaled = cv2.applyColorMap(depth_scaled, cv2.COLORMAP_RAINBOW)  # apply color map for pretty colors
 
             print(t)
