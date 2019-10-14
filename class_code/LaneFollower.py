cc90bb6fe6e35d610da512c1356656e966a0e83f
Benjamin Jensen <benjjensen@Live.com>
2019-10-14 16:35:02 -0600

    Deleted lane Testing and updated lane follower

--- class_code/LaneFollower.py
+++ class_code/LaneFollower.py
@@ -1,65 +1,81 @@
-# Top Level organization for navigation of autonomous car
-# Takes in an image and identifies the proper power and angle
 # IN TESTING - 10 Oct 2019!
 
-
-# Make a LaneFollower() class
-# Wrap lane follower in a try block
+# To do:
+#   - Wrap in a try block, if fails have it go straight and slow 
+#   - Tune parameters (Canny, Clean, etc...)
+#   - Crop image
+#   - Standardize coding style
 
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 import math
-from car_control import carControl
-
 
 class LaneFollower:
+    """
+    Works with an image to find lanes and determine a steering anble
+    """
 
     def __init__(self):
-        # Hashtag-Define's
         self.leftColorMin = np.asarray([90, 245, 180])        # Yellow - Determined by plotting imgHSV and hovering over the colors
         self.leftColorMax = np.asarray([100, 255, 210])       # Yellow
         self.rightColorMin = np.asarray([5, 15, 170])         # White
         self.rightColorMax = np.asarray([20, 40, 230])        # White
-        self.runNavigation = True
-
-        self.croppedHeightRatio = (1.0/2.0)
-        self.minSlope = 0.4
+        self.croppedHeightRatio = (1.0/2.0)     # Dimensions of the cropped image
+        self.minSlope = 0.4      # Used to filter out lines that couldn't be the lanes
+        self.max_angle = 30      # Maximum steering angle
+            # NOT USED CURRENTLY #
         self.carCenter = 772     # X value of center of the car, as camera is offcenter
 
+    def update_picture(self, img):
+        """
+        Takes in an image and updates the dimension values
+        """
+        self.raw_image = img                    # Stores a copy of the raw image
+        self.imgHeight = img.shape[0]
+        self.imgWidth = img.shape[1]
+            # MAY NEED TO BE UPDATED TO CAR CENTER #
+        self.center_point = (int(self.imgWidth/2), self.imgHeight) 
 
-    # def processImage(rawImg):
-    #     imgHeight = rawImg.shape[0]
-    #     imgWidth = rawImg.shape[1]
+    def clean_image(self, img):
+        """
+        UNDER TESTING !!!
+        Smooths out image. 
+        Values need to be tuned
+        """
+        kernel = np.ones((5,5), up.uint8)
+        return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-    #     # Clean image. May not actually do anything
-    #     kernel = np.ones((5,5), np.uint8)       # Not exactly sure how to pick values for this, this was just a recommendation I found...
-    #     cleanImg = cv2.morphologyEx(rawImg, cv2.MORPH_OPEN, kernel)
-    #     cleanImg = cv2.cvtColor(cleanImg, cv2.COLOR_BGR2RGB)
+    def convert_to_HSV(self, img):
+        """
+        Converts a RGB image to HSV
+        """
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
-    #     imgHSV = cv2.cvtColor(cleanImg, cv2.COLOR_BGR2HSV) # Convert to HSV
+    def filter_by_color(self, imgHSV, color_min, color_max):
+        """ 
+        Searches through an HSV image and filters out all pixels not in 
+        the specified range of values.
+        """
+        min_array = np.asarray([color_min])
+        max_array = np.asarray([color_max])
+        return cv2.inRange(imgHSV, min_array, max_array)
+ 
+    def canny_img(self, img):
+        """
+        Finds all edges in an image
+        """
+        return cv2.Canny(img, 100, 200)     # Lower and upper thresholds not chosen for any specific reason
 
-    #     leftLineImg = cv2.inRange(imgHSV, self.leftColorMin, self.leftColorMax)
+    def crop_image(self, img):
+        """
+        Takes in an image and crops out a specified range
+        """
+        cropVertices = [(0, self.imgHeight),                      # Corners of cropped image
+            (0, self.imgHeight * self.croppedHeightRatio),        # Gets bottom portion
+            (self.imgWidth, self.imgHeight * self.croppedHeightRatio),
+            (self.imgWidth, self.imgHeight) ] 
 
-    #     rightLineImg = cv2.inRange(imgHSV, self.rightColorMin, self.rightColorMax)
-
-    #         # Canny
-    #     leftCannyed = cv2.Canny(leftLineImg, 100, 200)          # Lower and upper thresholds chosen somewhat arbitrarily
-    #     rightCannyed = cv2.Canny(rightLineImg, 100, 200)
-
-    #     return [leftCannyed, rightCannyed]
-
-    def calculateAngle(c, p1, p2):                    # AKA Inner Product
-        c = np.asarray(c)
-        p1 = np.asarray(p1)
-        p2 = np.asarray(p2)
-                                                # (P1 dot P2) / |P1| * |P2|
-        angle = np.arccos( (np.dot(p1, p2)) /
-                            (np.linalg.norm(p1) * np.linalg.norm(p2)) )
-
-        return angle
-
-    def regionOfInterest(img, vertices):
         # Blank matrix that matches the image height/width
         mask = np.zeros_like(img)
 
@@ -67,146 +83,118 @@ class LaneFollower:
         # channel_count = img.shape[2] # Number of color channels      -> Same as below
         # match_mask_color = (255,) * channel_count # Matches color    -> Commented out for grayscale
 
-        cv2.fillPoly(mask, vertices, match_mask_color) # Fill polygon
+        cv2.fillPoly(mask, np.array([cropVertices], np.int32), match_mask_color) # Fill polygon
 
         masked_image = cv2.bitwise_and(img, mask)
 
         return masked_image
 
-    def findIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
-        tNumerator   = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)
-        tDenominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+    def find_intersection(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        """
+        Takes in two points on two lines (4 points total)
+        Determines the slope and then uses the given points to calculate 
+            an intersection point.
+        """
 
-        t = tNumerator/tDenominator
+        t_numerator   = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)
+        t_denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+
+        t = t_numerator/t_denominator
 
         intX = x1 + t * (x2 - x1)
         intY = y1 + t * (y2 - y1)
 
-        intPoint = (int(intX), int(intY))
-        return intPoint
+        int_point = (int(intX), int(intY))
+        return int_point
 
-    def updateController(rightProcessed, leftProcessed):
-        # Crop
-        cropVertices = [(0, imgHeight),               # Corners of cropped image
-                        (0, imgHeight * self.croppedHeightRatio),        # Gets bottom third
-                        (imgWidth, imgHeight * self.croppedHeightRatio),
-                        (imgWidth, imgHeight) ]
+    def hough_lines(self, img):
+        """
+        Takes in edges and connects the pixels into lines
+        PARAMETERS CAN BE ADJUSTED
+        """
+        return cv2.HoughLinesP(
+                img,
+                rho = 6,
+                theta = np.pi/60,
+                threshold = 100,
+                lines = np.array([]),
+                minLineLength = 20,
+                maxLineGap = 80)
 
-        leftCropped = regionOfInterest(leftProcessed, np.array([cropVertices], np.int32))
-        rightCropped = regionOfInterest(rightProcessed, np.array([cropVertices], np.int32))
-
-        # Hough Transforms to find lines -> Note that leftLine arguments is more lenient
-        leftLines = cv2.HoughLinesP(
-            leftCropped,
-            rho = 6,
-            theta = np.pi/60,
-            threshold = 100,
-            lines = np.array([]),
-            minLineLength = 20,
-            maxLineGap = 80)
-
-        rightLines = cv2.HoughLinesP(
-            rightCropped,
-            rho = 6,
-            theta = np.pi/60,
-            threshold = 150,
-            lines = np.array([]),
-            minLineLength = 60,
-            maxLineGap = 40)
-
+    def find_lines(self, left_lines, right_lines):
+        """
+        Takes in a list of lines and sorts through to find the lanes
+        """
         # Filter out lines and group remaining
-        leftLine_x = []
-        leftLine_y = []
-        rightLine_x = []
-        rightLine_y = []
+        left_line_x = []
+        left_line_y = []
+        right_line_x = []
+        right_line_y = []
 
-        for line in leftLines:
+
+        for line in left_lines:
             for x1, y1, x2, y2 in line:
                 slope = (y2-y1) / (x2-x1)
                 if math.fabs(slope) > self.minSlope:          # Ignore lines with slopes that aren't what we are looking for
-                    leftLine_x.extend([x1, x2])          # Add valid lines to the list
-                    leftLine_y.extend([y1, y2])
+                    left_line_x.extend([x1, x2])                # Add valid lines to the list
+                    left_line_y.extend([y1, y2])
 
-        for line in rightLines:
+        for line in right_lines:
             for x1, y1, x2, y2 in line:
                 slope = (y2-y1) / (x2 - x1)
-                if ((math.fabs(slope) > self.minSlope) & (slope>=0)):
-                    rightLine_x.extend([x1, x2])
-                    rightLine_y.extend([y1, y2])
+                if ((math.fabs(slope) > self.minSlope) & (slope>=0)):   # Ignore lines with slopes that are invalid
+                    right_line_x.extend([x1, x2])
+                    right_line_y.extend([y1, y2])
 
+        return left_line_x, left_line_y, right_line_x, right_line_y
 
-        # Calculate and draw left line
-        leftLine = np.polyfit(leftLine_y, leftLine_x, 1)
-        lspace = np.linspace(0, imgHeight, 10)
+    def calculate_lines(self, img, line_x, line_y, order):
+        """
+        Takes in a list of x and y coordinates and uses them to fit a line of a designated order
+        """
+        line = np.polyfit(line_y, line_x, 1)
+        lspace = np.linspace(0, self.imgHeight, 10)     # Generates the line for the whole image height
         drawY = lspace
-        drawX = np.polyval(leftLine, drawY)       # May cause a problem if not a real function
-        leftPoints = (np.asarray([drawX, drawY]).T).astype(np.int32)
-        finalLeft = cv2.polylines(rawImg, [leftPoints], False, (255, 0, 225), thickness = 10)
+        drawX = np.polyval(line, drawY)                 # May cause a problem if not a real function
+        points = (np.asarray([drawX, drawY]).T).astype(np.int32)                    # Points on line
+        final = cv2.polylines(img, [points], False, (255, 0, 225), thickness = 10)  # Draws lines
 
-        # Calculate and draw right line
-        rightLine = np.polyfit(rightLine_y, rightLine_x, 1)
-        drawX = np.polyval(rightLine, drawY)
-        rightPoints = (np.asarray([drawX, drawY]).T).astype(np.int32)
-        finalImg = cv2.polylines(finalLeft, [rightPoints], False, (255, 0, 255), thickness = 10)
+        return final, points
 
-        # Calculate intersection point
-        intPoint = findIntersection(leftPoints[0][0], leftPoints[0][1],
-                                    leftPoints[1][0], leftPoints[1][1],
-                                    rightPoints[0][0], rightPoints[0][1],
-                                    rightPoints[1][0], rightPoints[1][1])
+    def plot_center(self, img, intersection_point):
+        """
+        Draws a line from the center of the screen to the intersection points of the lanes
+        """
+        # centerPoint = (self.carCenter, imgHeight)     # Used if we are accounting for the camera offset
+        cv2.line(img, (self.center_point), (intersection_point[0], intersection_point[1]), (0,255,0), 5)
 
-        # Draw line from center to intersection point
-        centerPoint = (self.carCenter, imgHeight)
-        cv2.line(finalImg, (centerPoint), (intPoint[0], intPoint[1]), (0,255,0), 5)
+        return img
 
-        # Calculate angle off of center
-        midPoint = (self.carCenter, int(imgHeight/2))              # Center of screen. Used as a vertical reference for angle of departure
-        cv2.line(finalImg, (centerPoint), (midPoint), (100, 200, 200), 4)
-        angle = calculateAngle(centerPoint, intPoint, midPoint)
+    def saturate(self, angle):
+        """
+        Takes in an angle and ensures it is within the permitted range
+        """
+        if (abs(angle) > self.max_angle):
+            angle = self.max_angle * np.sign(angle)
+        return angle
 
-        angle = angle * 180/np.pi # is this angle * 180/pi instead of 180/pi???
+    def calculate_angle(self, intersection_point):                      # Uses inner product
+        """
+        Uses the intersection point, center point, and midpoint of the screen to calculate 
+            the angle (uses an inner product)
+        """
 
-        return [angle, finalImg]
+        # Determines the points and formats as arrays
+        mid_point = (int(self.imgWidth/2), int(self.imgHeight/2))
+        c = np.asarray(self.center_point)
+        p1 = np.asarray(intersection_point)
+        p2 = np.asarray(mid_point)    
 
-    def runLaneDetection(self):
+        angle = np.arccos( (np.dot(p1, p2)) /           # (P1 dot P2) / |P1| * |P2|
+                            (np.linalg.norm(p1) * np.linalg.norm(p2)) )
 
-        car = carControl()
-        car.drive(0.7)
+        angle = angle * 180/np.pi                       # Convert to degrees
 
-        while (self.runNavigation):
-            # Get image
-            car.update_sensors()
-            time, rawImg = car.get_rgb_data()
+        angle = self.saturate(angle)                    # Saturate angle
+        return angle
 
-            imgHeight = rawImg.shape[0]
-            imgWidth = rawImg.shape[1]
-
-            # Clean image. May not actually do anything
-            kernel = np.ones((5,5), np.uint8)       # Not exactly sure how to pick values for this, this was just a recommendation I found...
-            cleanImg = cv2.morphologyEx(rawImg, cv2.MORPH_OPEN, kernel)
-            cleanImg = cv2.cvtColor(cleanImg, cv2.COLOR_BGR2RGB)
-
-            imgHSV = cv2.cvtColor(rawImg, cv2.COLOR_BGR2HSV) # Convert to HSV
-
-            leftLineImg = cv2.inRange(imgHSV, self.leftColorMin, self.leftColorMax)
-
-            rightLineImg = cv2.inRange(imgHSV, self.rightColorMin, self.rightColorMax)
-
-                # Canny
-            leftCannyed = cv2.Canny(leftLineImg, 100, 200)          # Lower and upper thresholds chosen somewhat arbitrarily
-            rightCannyed = cv2.Canny(rightLineImg, 100, 200)
-
-            # [rightProcessed, leftProcessed] = processImage(frame)
-            [angle, finalImg] = updateController(rightCannyed, leftCannyed)
-
-            if angle > 30:
-                angle = 29
-            elif angle < -30:
-                angle = -29
-
-            car.steer(angle)
-
-            # print(angle)
-
-            # plt.imshow(finalImg)
-            # plt.show()
