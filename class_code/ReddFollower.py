1ea1a5dd2b1e6b637bc99b3fd1af290982b66cb8
kalin <kalinnorman@gmail.com>
2019-10-19 14:09:57 -0600

    Tuning Saturday right lane based

--- class_code/ReddFollower.py
+++ class_code/ReddFollower.py
@@ -13,7 +13,8 @@ class ReddFollower:
         self.car_control_speed = 0.4
         self.car_control_steering_angle = 0.0
         # self.birdseye_transform_matrix = np.load('car_perspective_transform_matrix_short_range.npy')
-        self.birdseye_transform_matrix = np.load('car_perspective_transform_matrix_short_range_warp.npy')
+        # self.birdseye_transform_matrix = np.load('car_perspective_transform_matrix_short_range_warp.npy')
+        self.birdseye_transform_matrix = np.load('car_perspective_transform_matrix_warp_2.npy')
         self.theta_left_base = -0.5
         self.theta_right_base = 0.0
         self.counts = [0,0,0]
