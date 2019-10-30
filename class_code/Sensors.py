2e2442c847153612426fdee12d70c3f3d05270f0
Benjamin Jensen <benjjensen@Live.com>
2019-10-30 07:49:40 -0600

    Replacing files

--- class_code/Sensors.py
+++ class_code/Sensors.py
@@ -1,38 +1,28 @@
 """
 Sensors.py
-
 A class for handling sensor data
-
 This includes camera, IMU, GPS, YOLO3...
-
 Functions & parameters:
 init()
 get_all_data()
-
 # Functions to access camera/IMU data
 get_depth_data()
 get_rgb_data()
 get_gyro_data()
 get_accel_data()
-
 # Functions for GPS
 get_region()
-
 # Functions to access yolo data
 get_class_id()
 get_score()
 get_bb()
-
 # Functions for accessing IMU data
 gyro_data(gyro)
 accel_data(accel)
 update_sensors()
-
 # Functions for GPS
 get_gps_region()
 get_gps_coord(color)
-
-
 Author: KAB'B
 """
 
