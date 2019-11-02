0093eed747d27c4c7e36bbafa09edc48ba5e529a
Benjamin Jensen <benjjensen@Live.com>
2019-11-02 13:47:13 -0600

    Added in deleteMe files...

--- class_code/main.py
+++ class_code/main.py
@@ -3,6 +3,7 @@ demo_steering.py
 For testing our lane following algorithm.
 """
 
+import pyrealsense2 as rs
 from CarControl import CarControl
 import time
 import random
@@ -35,6 +36,39 @@ def turn_handler(cc):
             cc.action.turn_right_while_moving()
     else:
         print("I haven't a clue where I am.")
+
+def crop_image(img):
+        """
+        Takes in an image and crops out a specified range
+        """
+        cropVertices = [(25, 0),                      # Corners of cropped image
+                        (75, 163),        # Gets bottom portion
+                        (120, 163),
+                        (160, 0)] 
+
+        # Blank matrix that matches the image height/width
+        mask = np.zeros_like(img)
+
+        match_mask_color = 255 # Set to 255 to account for grayscale
+        # channel_count = img.shape[2] # Number of color channels      -> Same as below
+        # match_mask_color = (255,) * channel_count # Matches color    -> Commented out for grayscale
+
+        cv2.fillPoly(mask, np.array([cropVertices], np.int32), match_mask_color) # Fill polygon
+
+        masked_image = cv2.bitwise_and(img, mask)
+
+        return masked_image
+
+def detect_object(img):
+    height = img.shape[0]
+    width = img.shape[1]
+
+    for y in range(124, 164):
+        for x in range(25, 160):
+            if img[y][x] != 0:
+                return True
+
+    return False
 
 # def turn_with_counter(type_of_turn):
 #     static type_of_turn = 'i'
@@ -71,6 +105,16 @@ def turn_handler(cc):
 
 if __name__ == '__main__':
 
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    birdseye_transform_matrix = np.load('car_perspective_transform_matrix_warp_2.npy')
+
+    # Start streaming
+    pipeline.start(config)
+
     cc = CarControl()  # create object to control car
     count = 0  # debouncer for finding limit lines
     speed = 0.3 # 0.3
@@ -86,6 +130,46 @@ if __name__ == '__main__':
     
         count = 0
         while True:
+
+            #####
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
+
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+            # Stack both images horizontally
+            # images = np.hstack((color_image, depth_colormap))
+
+            birdseye_frame = cv2.warpPerspective(depth_colormap, birdseye_transform_matrix, (200,200))
+
+            bCanny = cv2.Canny(birdseye_frame, 50, 200)
+
+            cropped_image  = crop_image(bCanny)
+
+
+
+            # Show images
+            # plt.imshow(cropped_image)
+            # plt.show()
+            objectFound = False
+            objectFound = detect_object(cropped_image)
+
+            if objectFound:
+                cc.drive(0.0)
+                continue
+
+            #####
+
+
             count += 1
             cc.update_sensors()  # update the sensors every loop
             t, rgb = cc.sensor.get_rgb_data()  # get color image
