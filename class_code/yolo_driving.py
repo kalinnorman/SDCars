d684a22cd432902f75f8e82dabeb0bb96e768434
atwitchell <atwitch23@gmail.com>
2019-11-21 22:48:52 -0700

    implements the yolo functionality; still need to modify colors and incorporate into driving code

--- class_code/yolo_driving.py
+++ class_code/yolo_driving.py
@@ -3,9 +3,25 @@ driving with yolo
 '''
 
 import cv2
+from matplotlib import pyplot as plt
+import numpy as np
 from yolo_4_autumn import Yolo
 
+# # defines
+lower_red = np.array([30,150,200])      # I need to test these values
+upper_red = np.array([255,255,255])
+lower_green = np.array([30,150,50])
+upper_green = np.array([255,255,180])
+lower_yellow = np.array([30,150,179])
+upper_yellow = np.array([255,255,180])
+yolo_map = cv2.imread('Maps/yolo_regions.bmp')
+yolo_region = 123
+yo = Yolo()
+img_middle = 208    # this is the middle of the yolo picture, the width is always 416 pixels
+yolo_frame_count = 0    # we use this so that we aren't checking yolo at every frame; probably should put this in Sensors.py
+color_detected = 255  # used to detect white in the mask pictures
 
+# this function is already in the driving code
 def get_gray_value(coordinates, img): # Converts from cv2 coords to coords on Dr Lee's image
     imgWidth = img.shape[1] # Get width
     x = round(coordinates[0]) # x translates directly
@@ -15,71 +31,92 @@ def get_gray_value(coordinates, img): # Converts from cv2 coords to coords on Dr
     cur_gray_val = gray_val
     return gray_val
 
-def crop_image(img, box):
-    """
-    Takes in an image and crops out a specified range
-    """
-    cropVertices = [(box[0], box[1]),                      # Corners of cropped image
-        (box[1], box[2]),        # Gets bottom portion
-        (box[2], box[3]),
-        (box[3], box[4]) ]
 
-
-yo = Yolo()
 # driving stuff
 
 # before anything else, check that we're in the region for yolo
 # if we are, check for yolo (have a counter for each frame so you aren't checking
 # it at every frame)
 
-yolo_map = cv2.imread('Maps/yolo_regions.bmp')
-check_yolo_region = 123
+coordinates = (300,600) # this is for testing purposes; it's in the NE of the yolo regions
+if get_gray_value(coordinates, yolo_map)[0] == yolo_region :
+    yolo_frame_count = 10 #+= 1
 
-cv2.circle(yolo_map, (600,300) ,5, color=(0,255,0))
+    if yolo_frame_count == 10 :
+        yolo_frame_count = 0
+        # this is the equivalent to reading in a frame
+        imgLight = cv2.imread('traffic_lights/red_light2.png')
 
-#print(get_gray_value((600,300), yolo_map)[0])
-if get_gray_value((300,600), yolo_map)[0] == check_yolo_region :
-    print('DEBUG: oh yeah!')
+        cv2.imshow("light", imgLight)
+        cv2.waitKey(0)
+        # do yolo
+        # get vector of bounding boxes from yolo and check that they're on the right side
+        bounding_boxes, yolo_img = yo.main_yolo(imgLight)
+        light_boxes = []
+        # bounding_box = [x1, y1, x2, y2]   # format of bounding_boxes[i]
+        for box in range(0, len(bounding_boxes)):
+            # x1 = bounding_boxes[box][0]
+            # x2 = bounding_boxes[box][2]
 
-# cv2.imshow('map', yolo_map)
-# cv2.waitKey(0)
+            if bounding_boxes[box][0] > img_middle and bounding_boxes[box][2] > img_middle :  # bounding box is on the right side of the camera
+                light_boxes.append(bounding_boxes[box])
+                print (light_boxes[-1])
+        y_of_light = 400 # arbitrary value that is used to compare when there is more than one detected traffic light
+        if not light_boxes:
+            print("DEBUG: oh no! there aren't any boxes!") # exit frame and try again
+        # we only want to look at one light, so if we detect more than one,
+        # we will look at the traffic light that is closest to the top of the pic as
+        # that one is likely to be the one we want to look at
+        elif len(light_boxes) > 1 :
+            for i in range(0, len(light_boxes)) :
+                top_y = min(light_boxes[i][1], light_boxes[i][3])
+                if top_y < y_of_light :
+                    y_of_light = top_y
+                    desired_light = i
+        else :
+            desired_light = 0   # there's only one traffic light detected in the desired region
+        ################## I need to double check that y = 0 is the top ############
 
-imgLight = cv2.imread('traffic_lights/red_light2.png')
-cv2.rectangle(imgLight, (364, 161), (383, 194), (0,255,0))
+        # crop image:
+        x1 = int(light_boxes[desired_light][0])
+        y1 = int(light_boxes[desired_light][1])
+        x2 = int(light_boxes[desired_light][2])
+        y2 = int(light_boxes[desired_light][3])
+        cropped_img = yolo_img[y1:y2, x1:x2]
 
-cv2.imshow("light", imgLight)
-cv2.waitKey(0)
+        imgHSV = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2HSV)
 
-# img_middle = int(imgLight.shape[1]/2)
+        maskRed = cv2.inRange(imgHSV, lower_red, upper_red)
+        maskGreen = cv2.inRange(imgHSV, lower_green, upper_green)
+        maskYellow = cv2.inRange(imgHSV, lower_yellow, upper_yellow)
 
-# put in Sensors.py: self.yolo_count = 0
-# if it's in the four regions, # look at Maps/regions.bmp
-# run yolo
-# check that traffic light is on the right side
-# get bounding coordinates
+        red_detection = 0
+        yellow_detection = 0
+        green_detection = 0
 
-img_middle = 208    # for some reason this isn't the same as half the camera size, but oh well!
+        # parse through masked images and check for white
+        # whichever mask has the highest amount of white is the winner
+        for y in range(0, y2-y1) :
+            for x in range(0, x2-x1) :
+                if maskRed[y][x] == color_detected :
+                    red_detection += 1
+                if maskYellow[y][x] == color_detected :
+                    yellow_detection += 1
+                if maskGreen[y][x] == color_detected :
+                    green_detection += 1
+        if red_detection > green_detection and red_detection > yellow_detection :
+            detection = 'red'
+        elif yellow_detection > green_detection :
+            detection = 'yellow'
+        else :
+            detection = 'green'
 
-coordinates = (300,600)
-if get_gray_value(coordinates, yolo_map)[0] == check_yolo_region :
-    # do yolo
-    # get vector of bounding boxes from yolo and check that they're on the right side
-    bounding_boxes = yo.main_yolo(imgLight)
-    light_boxes = []
-    # bounding_box = [x1, y1, x2, y2]
-    # bounding_box = [229, 138, 232, 145]
-    for box in range(0, len(bounding_boxes)):
-        x1 = bounding_boxes[box][0]
-        x2 = bounding_boxes[box][2]
+        print(detection, " is the winner!")
+        cv2.imshow("cropped", imgHSV)
+        cv2.imshow('red mask',maskRed)
+        cv2.imshow('yellow mask',maskYellow)
+        cv2.imshow('green mask',maskGreen)
 
-        if x1 > img_middle and x2 > img_middle :  # bounding box is on the right side of the camera
-            light_boxes.append(bounding_boxes[box])
-            print (light_boxes[-1])
-    if not light_boxes:
-        break # exit frame and try again
+        cv2.waitKey(0)
 
-    # figure out how to crop image and get color
-    # check what color is inside it
-    # what if there is more than one light being detected??
-    # guess that the closer one will be bigger?
-    # or whichever one is closest to the top of the image?
+        # return detection
