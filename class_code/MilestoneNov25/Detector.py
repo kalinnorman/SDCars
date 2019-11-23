d3cffaeef1e7ec6c56c3ef41a4d35b8fc371c09a
kalin <kalinnorman@gmail.com>
2019-11-23 11:17:58 -0700

    Added folder for next milestone

---
+++ class_code/MilestoneNov25/Detector.py
@@ -0,0 +1,85 @@
+""" Object Detector
+Class that is used to detect objects in the close field of vision.
+Gets an image, transforms it to birdseye view, cannies it, and then crops it.
+A specified portion of the cropped image is searched for non-zero values (meaning an object)
+
+Date: 2 Nov 2019
+Author: Benj
+"""
+
+import pyrealsense2 as rs 
+import numpy as np 
+import cv2 
+from matplotlib import pyplot as plt 
+
+class Detector:
+    """
+    This class is used to detect objects
+    """
+
+    def __init__(self, sensor):
+
+        self.sensor = sensor       # Passed in by CarControl.py
+        self.birdseye_transform_matrix = np.load('car_perspective_transform_matrix_warp_2.npy')
+        self.count = 0
+        self.debuggerCount = 3
+
+    def crop_image(self, img):
+        """
+        Takes in an image and crops out a specified range.
+        In this case, we are cropping a trapezoid out of the birdseye view
+        """
+        cropVertices = [(25, 0),          # Corners of trapezoid to crop
+                        (75, 163),        
+                        (120, 163),
+                        (160, 0)] 
+
+    
+        mask = np.zeros_like(img) # Blank matrix that matches the image height/width
+        match_mask_color = 255    # Set to 255 to account for grayscale
+        cv2.fillPoly(mask, np.array([cropVertices], np.int32), match_mask_color) # Fill polygon
+
+        masked_image = cv2.bitwise_and(img, mask)
+
+        return masked_image
+
+    def search_range(self, img):
+        """
+        Searches through a picture for non-zero values, returning True if something is found
+        (Search area is in need of further tuning)
+        """
+
+        for y in range(100, 180):#90,180       # 115 <= y <= 164
+            for x in range(75, 110):    # 25 <= x <= 160
+                if img[y][x] != 0:
+                    return True
+
+        return False
+
+    def detect_object(self):
+        """
+        Main class method
+        Loads in a depth image, converts to birdseye view, Cannies, and then crops.
+        """
+        try:
+            time, depth_image = self.sensor.get_depth_data()
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+            birdseye_frame = cv2.warpPerspective(depth_colormap, self.birdseye_transform_matrix, (200, 200))
+            cannied_image = cv2.Canny(birdseye_frame, 50, 200) 
+
+            cropped_image = self.crop_image(cannied_image)
+
+            object_found = self.search_range(cropped_image)
+            
+            if (object_found):
+                self.count = self.count + 1
+                if (self.count >= self.debuggerCount):
+                   # self.count = 0
+                    return True, cropped_image
+            else:
+                self.count = 0
+            return False, cropped_image
+        except:
+            print("Detect Image Failed")
+            return False, 0
---
+++ class_code/MilestoneNov25/Drive.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from Follower import Follower
+from Control import Control
+import Parameters as gp
+import numpy as np
+import queue
+import time
+import math
+import cv2
+import sys
+
+# Setup
+car = Follower()  # initialize the car
+car.cc.steer(0)  # set the steering to straight
+prev_region = car.cur_region
+next_region = car.desired_region
+cur_img = car.predict.map
+car.cc.drive(car.speed)
+try:
+    while True:
+        # Get new GPS Coordinate
+        while car.cur_gps == car.prev_gps: # Wait for new gps coordinate
+            # FIXME insert object detection behavior here!
+            car.update_gps_pos()
+            if car.prev_gps[0] < 0: # Updates the prev gps if the car was out of bounds but reentered
+                car.prev_gps = car.cur_gps
+        # Make steering decisions if a valid GPS coordinate was returned
+        if car.cur_gps[0] > 0:
+            car.update_region()
+            # First case: Entering the intersection from any region
+            if prev_region != gp.region_dict['Intersection'] and car.cur_region == gp.region_dict['Intersection']: 
+                cur_img, next_region = car.get_intersection_map(prev_region)  # use the appropriate map to turn
+                car.predict.set_img(cur_img)
+                prev_region = car.cur_region
+            # Second case: Leaving the intersection
+            elif prev_region == gp.region_dict['Intersection'] and car.cur_region != gp.region_dict['Intersection']:
+                if next_region == gp.region_dict['Region 1'] or next_region == gp.region_dict['Region 4']:
+                    cur_img = car.regions1and4 
+                else:
+                    cur_img = car.regions2and3
+                car.predict.set_img(cur_img)
+                prev_region = next_region
+            # Third Case: Car is located in the region it is meant to be in for driving commands
+            elif prev_region == car.cur_region:
+                if car.check_stop_signs(): # If at a stop sign location
+                    car.cc.steer(0)
+                    car.cc.drive(0)
+                    time.sleep(2)
+                    car.cc.drive(car.speed)
+                # FIXME elif for YOLO behavior will go here
+                # FIXME if we do need to stop at the light I also need to add in a good way to detect where that is...
+                elif car.cur_region == car.desired_region:
+                    car.update_desired_region()
+            steering_angle = car.get_angle()
+            car.cc.steer(steering_angle)
+            car.prev_gps = car.cur_gps
+
+        else:  # if the gps didn't find us
+            car.cur_region = gp.region_dict['Out of bounds']  # indicate we are out of bounds
+            car.prev_gps = car.cur_gps
+           
+        car.update_log_file()  # update the log file
+    
+except KeyboardInterrupt:  # if the user Ctrl+C's us
+    print("User Terminated Program")
+    car.out_file.close()  # close the log file
