89da391c7603fc827d6040835f69f085df3b725c
ae6nr <bryan.d.redd@gmail.com>
2019-11-25 17:51:11 +0000

    Moving back to add code

--- class_code/colorTesting.py
+++ class_code/colorTesting.py
@@ -4,10 +4,10 @@ import cv2
 
 import pyrealsense2 as rs 
 
-colorMin_1 = np.asarray([0, 0, 0])
-colorMax_1 = np.asarray([255, 10, 10])
-colorMin_2 = np.asarray([0, 0, 0])
-colorMax_2 = np.asarray([10, 255, 10])
+colorMin_1 = np.asarray([230, 0, 0])
+colorMax_1 = np.asarray([255, 255, 255])
+colorMin_2 = np.asarray([0, 240, 0])
+colorMax_2 = np.asarray([255, 255, 255])
 colorMin_3 = np.asarray([0, 0, 0])
 colorMax_3 = np.asarray([10, 10, 255])
 
@@ -20,20 +20,31 @@ pipeline.start(config)
 
 try:
     while True:
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        if not color_frame:
-            continue
+        for x in range(10):
+            frames = pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            if not color_frame:
+                continue
 
         color_image = np.asanyarray(color_frame.get_data())
-        color_frame = cv2.applyColorMap(cv2.convertScaleAbs(color_frame, alpha= 0.03), cv2.COLORMAP_JET)
+#        color_frame = cv2.applyColorMap(cv2.convertScaleAbs(color_frame, alpha= 0.03), cv2.COLORMAP_JET)
 
-        img = cv2.cvtColor(color_frame, cv2.COLOR_BGR2HSV)
+#        img = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+        img = color_image
         mask_image_1 = cv2.inRange(img, colorMin_1, colorMax_1)
         mask_image_2 = cv2.inRange(img, colorMin_2, colorMax_2)
         mask_image_3 = cv2.inRange(img, colorMin_3, colorMax_3)
-
+ 
+        #cv2.imshow("vid",img)
+        #cv2.waitKey(0)
+       
+        plt.imshow(img)
+        plt.show()
         imageStack = np.hstack((mask_image_1, mask_image_2, mask_image_3))
-
-        cv2.imshow("image", imageStack)
-        key=cv2.waitKey(0)
+       
+        plt.imshow(imageStack)
+        plt.show()
+        #cv2.imshow("image", imageStack)
+        #cv2.waitKey(0)
+finally:
+    pipeline.stop()
