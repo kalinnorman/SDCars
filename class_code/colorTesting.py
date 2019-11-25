d4693a1bc15548de27b6abf579b5616484e7a638
ae6nr <bryan.d.redd@gmail.com>
2019-11-25 18:06:15 +0000

    Seems to get red and green

--- class_code/colorTesting.py
+++ class_code/colorTesting.py
@@ -6,10 +6,10 @@ import pyrealsense2 as rs
 
 colorMin_1 = np.asarray([230, 0, 0])
 colorMax_1 = np.asarray([255, 255, 255])
-colorMin_2 = np.asarray([0, 240, 0])
-colorMax_2 = np.asarray([255, 255, 255])
-colorMin_3 = np.asarray([0, 0, 0])
-colorMax_3 = np.asarray([10, 10, 255])
+colorMin_2 = np.asarray([0, 220, 0])
+colorMax_2 = np.asarray([100, 255, 100])
+colorMin_3 = np.asarray([0, 0, 230])
+colorMax_3 = np.asarray([60, 60, 255])
 
 
 pipeline = rs.pipeline()
@@ -47,8 +47,9 @@ try:
         color_image = np.asanyarray(color_frame.get_data())
 #        color_frame = cv2.applyColorMap(cv2.convertScaleAbs(color_frame, alpha= 0.03), cv2.COLORMAP_JET)
 
-#        img = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
-        img = color_image
+        img = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+#        img = crop_image(color_image)
+#        img = color_image
         mask_image_1 = cv2.inRange(img, colorMin_1, colorMax_1)
         mask_image_2 = cv2.inRange(img, colorMin_2, colorMax_2)
         mask_image_3 = cv2.inRange(img, colorMin_3, colorMax_3)
