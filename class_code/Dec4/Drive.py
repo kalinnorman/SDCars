dd15c7cf7ed83545e148295ac945de814bb16c71
Kalin Norman <kalinnorman@gmail.com>
2019-12-03 11:52:03 -0700

    Added code for the next milestone, began messing with timing how frequently the loop runs

---
+++ class_code/Dec4/Drive.py
@@ -0,0 +1,113 @@
+from datetime import datetime
+from Follower import Follower
+from Control import Control
+import Parameters as gp
+import numpy as np
+import queue
+import time
+import math
+import cv2
+import sys
+
+# Setup
+car = Follower()  # initialize the car
+prev_region = car.cur_region
+next_region = car.desired_region
+cur_img = car.predict.map
+car.start_car()
+# car.cc.steer(0)  # set the steering to straight
+# car.cc.drive(car.speed)
+stopAtIntersection = True
+try:
+    while True:
+
+        car.cc.update_sensors()
+        object_detected, image = car.cc.detector.detect_object()
+
+        if (object_detected):
+            if (not car.restart_car): # meaning the car is just stopping
+                car.stop_time = time.time()
+            car.stop_car()
+            if ( (time.time() - car.stop_time) > car.attempt_time): # after a few seconds, try to fix
+                car.attempt_correction()
+            continue
+        if (car.restart_car):
+            car.start_car()
+
+        # Get new GPS Coordinate
+        while car.cur_gps == car.prev_gps: # Wait for new gps coordinate
+            # FIXME insert object detection behavior here!
+            car.cc.update_sensors()
+            object_detected, image = car.cc.detector.detect_object()
+
+            if (object_detected):
+                if (not car.restart_car): # meaning the car is just stopping
+                    car.stop_time = time.time()
+                car.stop_car()
+                if ( (time.time() - car.stop_time) > car.attempt_time):  # after a few seconds, try to fix
+                    car.attempt_correction()
+                continue
+            if (car.restart_car):
+                car.start_car()
+
+
+            car.update_gps_pos()
+            if car.cur_gps[0] > 1024 or car.cur_gps[1] > 1600:
+                continue
+            elif car.cur_gps[0] < 0 or car.cur_gps[1] < 0:
+                continue
+#            print(car.get_gray_value(car.cur_gps, car.stops_and_lights)) # FIXME Delete this line once we have the gray value for YOLO
+            if car.prev_gps[0] < 0: # Updates the prev gps if the car was out of bounds but reentered
+                car.prev_gps = car.cur_gps
+        # Make steering decisions if a valid GPS coordinate was returned
+        if car.cur_gps[0] > 0:
+            car.update_region()
+            # First case: Entering the intersection from any region
+            if prev_region != gp.region_dict['Intersection'] and car.cur_region == gp.region_dict['Intersection']: 
+                cur_img, next_region = car.get_intersection_map(prev_region)  # use the appropriate map to turn
+                car.predict.set_img(cur_img)
+                prev_region = car.cur_region
+                car.cc.detector.inIntersection = True
+            # Second case: Leaving the intersection
+            elif prev_region == gp.region_dict['Intersection'] and car.cur_region != gp.region_dict['Intersection']:
+                if next_region == gp.region_dict['Region 1'] or next_region == gp.region_dict['Region 4']:
+                    cur_img = car.regions1and4 
+                else:
+                    cur_img = car.regions2and3
+                car.predict.set_img(cur_img)
+                prev_region = next_region
+                car.cc.detector.inIntersection = False
+            # Third Case: Car is located in the region it is meant to be in for driving commands
+            elif prev_region == car.cur_region:
+                if car.check_stop_signs(): # If at a stop sign location
+                    car.cc.steer(0)
+                    car.cc.drive(0)
+                    time.sleep(2)
+                    car.cc.drive(car.speed)
+                    stopAtIntersection = True
+                elif car.get_gray_value(car.cur_gps, car.intersectionStops) == 128 and stopAtIntersection:
+                    car.cc.steer(0)
+                    car.cc.drive(0)
+                    stopAtIntersection = False
+                    # FIXME (IMPLEMENT THE THREE LINES OF COMMENTED OUT CODE BENEATH THIS)
+                    while not car.cc.sensor.green_light:
+                        car.cc.update_sensors(yolo_flag=True)
+                    car.cc.drive(car.speed)
+                    car.cc.sensor.green_light = False
+                elif car.cur_region == car.desired_region:
+                    car.update_desired_region()
+            steering_angle = car.get_angle()
+            car.cc.steer(steering_angle)
+            car.prev_gps = car.cur_gps
+
+            
+
+        else:  # if the gps didn't find us
+            car.cur_region = gp.region_dict['Out of bounds']  # indicate we are out of bounds
+            car.prev_gps = car.cur_gps
+           
+        car.update_log_file()  # update the log file
+    
+except KeyboardInterrupt:  # if the user Ctrl+C's us
+    print("User Terminated Program")
+    car.out_file.close()  # close the log file
---
+++ class_code/Dec4/Sensors.py
@@ -0,0 +1,393 @@
+"""
+Sensors.py
+A class for handling sensor data
+This includes camera, IMU, GPS, YOLO3...
+Functions & parameters:
+init()
+get_all_data()
+# Functions to access camera/IMU data
+get_depth_data()
+get_rgb_data()
+get_gyro_data()
+get_accel_data()
+# Functions for GPS
+get_region()
+# Functions to access yolo data
+get_class_id()
+get_score()
+get_bb()
+# Functions for accessing IMU data
+gyro_data(gyro)
+accel_data(accel)
+update_sensors()
+# Functions for GPS
+get_gps_region()
+get_gps_coord(color)
+Author: KAB'B
+"""
+
+"""
+# YOLO USAGE
+# sudo MXNET_CUDNN_AUTOTUNE_DEFAULT=0 python3 yolo.py
+# OPTIONAL PARAMETERS
+# -c/--confidence (.0-1.0) (detected objects with a confidence higher than this will be used)
+"""
+
+import pyrealsense2 as rs
+import time
+import cv2
+import numpy as np
+import gc
+import requests # needed to for get_gps_coord()
+# from Yolo import Yolo
+# YOLO packages
+from matplotlib import pyplot as plt
+from gluoncv import model_zoo, utils
+import pyrealsense2 as rs
+from PIL import Image
+from signal import signal, SIGINT
+from sys import exit
+import numpy as np
+import mxnet as mx
+import argparse
+import imutils
+import serial
+import time
+import cv2
+import os
+import gc
+from matplotlib import pyplot as plt
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--confidence", type=float, default=0.4,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+"""Transforms for YOLO series."""
+def transform_test(imgs, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+    if isinstance(imgs, mx.nd.NDArray):
+        imgs = [imgs]
+    for im in imgs:
+        assert isinstance(im, mx.nd.NDArray), "Expect NDArray, got {}".format(type(im))
+
+    tensors = []
+    origs = []
+    for img in imgs:
+        orig_img = img.asnumpy().astype('uint8')
+        img = mx.nd.image.to_tensor(img)
+
+        img = mx.nd.image.normalize(img, mean=mean, std=std)
+
+        tensors.append(img.expand_dims(0))
+        origs.append(orig_img)
+    if len(tensors) == 1:
+        return tensors[0], origs[0]
+    return tensors, origs
+
+
+def load_test(filenames, short=416):
+    if not isinstance(filenames, list):
+        filenames = [filenames]
+    imgs = [letterbox_image(f, short) for f in filenames]
+    return transform_test(imgs)
+
+
+# this function is from yolo3.utils.letterbox_image
+def letterbox_image(image, size=416):
+    '''resize image with unchanged aspect ratio using padding'''
+    iw, ih = image.size
+
+    scale = min(size / iw, size / ih)
+    nw = int(iw * scale)
+    nh = int(ih * scale)
+
+    image = image.resize((nw, nh), Image.BICUBIC)
+    new_image = Image.new('RGB', (size, size), (128, 128, 128))
+    new_image.paste(image, ((size - nw) // 2, (size - nh) // 2))
+    return mx.nd.array(np.array(new_image))
+
+
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
+# vs = cv2.VideoCapture("/dev/video2", cv2.CAP_V4L)  # ls -ltr /dev/video*
+# (W, H) = (None, None)
+
+# Implement YOLOv3MXNet
+net = model_zoo.get_model('yolo3_mobilenet1.0_coco', pretrained=True)
+
+# Set device to GPU
+device = mx.gpu()
+
+net.collect_params().reset_ctx(device)
+
+print('Running. Press CTRL-C to exit')
+
+
+class Sensors():
+
+    def __init__(self):
+        # initialize variables that will contain the data
+        self.current_depth = None
+        self.current_rgb = None
+        self.current_gyro = None
+        self.current_accel = None
+        self.current_class_id = None
+        self.current_score = None
+        self.current_bb = [0, 0, 0, 0]
+        self.region = None
+
+        # initialize the video stream, pointer to output video file, and
+        # frame dimensions
+        self.vs = cv2.VideoCapture("/dev/video2", cv2.CAP_V4L)  # ls -ltr /dev/video*
+        self.writer = None
+        (self.W, self.H) = (None, None)
+
+        # Configure depth and color streams
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        self.config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 250)
+        self.config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)
+
+        # Start streaming
+        self.pipeline.start(self.config)
+
+        # YOLO
+        self.green_light = False
+        self.traffic_boxes = []
+        signal(SIGINT, self.handler)
+
+
+    # YOLO
+    # Function to correctly exit program
+    def handler(self, signal_received, frame):
+        self.vs.release()
+        cv2.destroyAllWindows()
+        print('CTRL-C detected. Exiting gracefully')
+        exit(0)
+
+    # # YOLO
+
+    def predict_color(self, img):
+        red_lower = np.array([0, 0, 230], dtype='uint8')
+        red_upper = np.array([60, 60, 255], dtype='uint8')
+
+        green_lower = np.array([0, 200, 0], dtype='uint8')
+        green_upper = np.array([100, 255, 150], dtype='uint8')
+        
+        imghsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+        redmask = cv2.inRange(imghsv, red_lower, red_upper)
+        greenmask = cv2.inRange(imghsv, green_lower, green_upper)
+
+        imgs = np.hstack((redmask, greenmask))
+        
+        count = cv2.countNonZero(greenmask)#[:,:,0])
+        print(count)
+        # plt.imshow(imgs)
+        # plt.show()
+        if count > 200 :
+            return 'green'
+        else:
+            return 'red'
+
+    # Data is from IMU, camera, and YOLO3
+    def get_all_data(self):
+        """
+        Returns all data as a list
+        """
+        return (time.time(),
+                self.current_depth,
+                self.current_rgb,
+                self.current_gyro,
+                self.current_accel,
+                self.current_class_id,
+                self.current_score,
+                self.current_bb)
+
+    # Functions to access camera/IMU data
+    def get_depth_data(self):
+        return time.time(), self.current_depth
+
+    def get_rgb_data(self): # gives a picture frame
+        return time.time(), self.current_rgb
+
+    def get_gyro_data(self):
+        return time.time(), self.current_gyro
+
+    def get_accel_data(self):
+        return time.time(), self.current_accel
+
+    # Functions for GPS
+    def get_region(self):
+        return time.time(), self.region
+
+    # Functions to access yolo data
+    def get_class_id(self):
+        return time.time(), self.current_class_id
+
+    def get_score(self):
+        return time.time(), self.current_score
+
+    def get_bb(self): # bounding box for YOLO3
+        return time.time(), self.current_bb
+
+    # Functions for accessing IMU data
+    def gyro_data(self, gyro):
+        return np.asarray([gyro.x, gyro.y, gyro.z])
+
+    def accel_data(self, accel):
+        return np.asarray([accel.x, accel.y, accel.z])
+
+    def update_sensors(self, yolo_flag=False):
+        # read the next frame from the file
+        (grabbed, frame) = self.vs.read()
+
+        # if the frame was not grabbed, then we have reached the end
+        # of the stream
+        if not grabbed:
+            raise Exception("No frame grabbed")
+            #break
+
+        # if the frame dimensions are empty, grab them
+        if self.W is None or self.H is None:
+            (self.H, self.W) = frame.shape[:2]
+
+        # start realsense pipeline
+        rsframes = self.pipeline.wait_for_frames()
+
+        # GPS data
+        self.region = self.get_gps_region()
+        coordinates = self.get_gps_coord("Blue")
+
+        #### Implement YOLOv3MXNet ####
+        if yolo_flag:
+            # from gluoncv import data
+            frame2 = frame[0:int(self.H/2), int(1*self.W/3):self.W] # cropping image
+
+            yolo_image = Image.fromarray(frame2, 'RGB')
+            
+            x, img = load_test(yolo_image, short=416)
+            cropimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            
+            class_IDs, scores, bounding_boxs = net(x.copyto(device))
+
+            # The next two lines draw boxes around detected objects
+            # ax = utils.viz.plot_bbox(cropimg, bounding_boxs[0], scores[0], class_IDs[0], class_names=net.classes)
+            # plt.show()
+
+            # Convert to numpy arrays, then to lists
+            class_IDs = class_IDs.asnumpy().tolist()
+            scores = scores.asnumpy().tolist()
+            bounding_boxs = bounding_boxs.asnumpy()
+            self.traffic_boxes = []
+
+            # iterate through detected objects
+            for i in range(len(class_IDs[0])):
+                if ((scores[0][i])[0]) > args["confidence"] or i == 0:
+                    current_class_id = net.classes[int((class_IDs[0][i])[0])]
+                    current_score = (scores[0][i])[0]
+                    self.current_bb = bounding_boxs[0][i]# - 1]
+                    # print("current_bb:",bounding_boxs[0][i])#-1])
+                    # print("ID:", current_class_id)
+                    if current_class_id == 'traffic light':
+                        self.traffic_boxes.append(self.current_bb)
+
+            if len(class_IDs[0]) == 0:
+                self.current_bb = [0, 0, 0, 0]
+                print("YOLO didn't find anything. :(")
+
+            light_boxes = []
+            # bounding_box = [x1, y1, x2, y2]   # format of bounding_boxes[i]
+            for box in range(0, len(self.traffic_boxes)):
+                light_boxes.append(self.traffic_boxes[box])
+                print(light_boxes[-1])
+            y_of_light = 400  # arbitrary value that is used to compare when there is more than one detected traffic light
+            if len(light_boxes) < 1:
+                print("DEBUG: oh no! there aren't any boxes!")  # exit frame and try again
+            else:
+                print("There are light boxes")
+                if len(light_boxes) > 1:
+                    for i in range(0, len(light_boxes)):
+                        top_y = min(light_boxes[i][1], light_boxes[i][3])
+                        if top_y < y_of_light:
+                            y_of_light = top_y
+                            desired_light = i
+                else:
+                    desired_light = 0  # there's only one traffic light detected in the desired region
+
+                # crop image:
+                x1 = int(light_boxes[desired_light][0])
+                y1 = int(light_boxes[desired_light][1])
+                x2 = int(light_boxes[desired_light][2])
+                y2 = int(light_boxes[desired_light][3])
+                cropped_img = cropimg[y1:y2, x1:x2]
+                # cv2.imshow("Traffic Light",cropped_img)
+                # cv2.waitKey(1)
+                if self.predict_color(cropped_img) == 'green' :
+                    self.green_light = True
+        #### END OF YOLO ####
+
+        # iterate through camera/IMU data, updating global variable
+        for rsframe in rsframes:
+            # Retrieve IMU data
+            if rsframe.is_motion_frame():
+                self.current_accel = self.accel_data(rsframe.as_motion_frame().get_motion_data())
+                self.current_gyro = self.gyro_data(rsframe.as_motion_frame().get_motion_data())
+            # Retrieve depth data
+            if rsframe.is_depth_frame():
+                depth_frame = rsframes.get_depth_frame()
+                # Convert to numpy array
+                depth_image = np.asanyarray(depth_frame.get_data())
+                self.current_depth = depth_image
+                self.current_rgb = frame
+
+        gc.collect()  # collect garbage
+
+    # call this when car has reached an intersection
+    def get_gps_region(self):
+        x,y = self.get_gps_coord("Blue")  # outputs coordinates (x,y)
+
+        # arbitrary values, need to test when have testing space
+        # we only care about the horizontal axis (y) for turning purposes
+        if y > 1400 :
+            region = 'north'
+            # should go left
+        elif y > 1200 :
+            region = 'middle north'
+            # should go right
+        elif y < 200 :
+            region = 'south'
+            # should go left
+        elif y < 500 :
+            region = 'middle south'
+            # should go right
+        else :
+            region = 'middle'
+            # can go any direction
+        return region
+
+    # retrieves the coordinates of a car (provided in class code)
+    # car color options are: "Green", "Red", "Purple", "Blue", "Yellow"
+    # my advice: pick "Blue" -ABT
+    def get_gps_coord(self, color):
+        # api-endpoint
+        success = False
+        while not success:
+            URL = "http://192.168.1.8:8080/%s" % color
+
+            # sending get request and saving the response as response object
+            r = requests.get(url = URL)
+
+            # extracting data
+            coorString = r.text
+            try:
+                coordinates = coorString.split()
+                latitude = float(coordinates[0])
+                longitude = float(coordinates[1])
+                success = True
+            except:
+                success = False
+
+        return (latitude, longitude)
