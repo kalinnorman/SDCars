a86829d145157840d4a8199f0c12ecc61b9f81d1
redd <bryan.d.redd@gmail.com>
2019-12-04 13:27:26 -0700

    show images for color detection

--- class_code/MilestoneNov25/Sensors.py
+++ class_code/MilestoneNov25/Sensors.py
@@ -165,7 +165,7 @@ class Sensors():
 
     # # YOLO
 
-    def predict_color(self, img, show_masks=False):
+    def predict_color(self, img, show_masks=True):
         """
         Updated by redd ot hopefully be more robust.
         Hopefully.
